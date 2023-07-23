48add53b1d7cff71f46398b35c3c68e7be884300
Andrea <giovanniroy49@gmail.com>
2023-07-23 18:27:10 -0400

    add pseudocode for renderAirCategories

--- main.js
+++ main.js
@@ -23,6 +23,27 @@ const getAirCategories = async () => {
 const airCategories = (airCategoriesObj) => {
   console.log("renderAirCategories");
   console.table(airCategoriesObj);
+  // find and select a UL with air quality categories ID dom element to append my data into
+
+  // for each element in our air qua;ity categories array
+  // create a wrapping element maybe <article> class of card
+  //appendChild the article.card to selected DOM element
+  //append the article.card to DOM
+  // display category name, image and description
+  // image
+  // create an image element
+  // set img src to category thumbnail url
+  // give class of category-thumbnail
+  // give it a mobile friendly max width of like 300px
+  //appendChild the image to our card
+  // name
+  // create an h4 element
+  // set h4 element textContent to be an air quality category name
+  //appendChild the image to our card
+  //description
+  // create an p element
+  // set p element textContent to be an air quality category name
+  //appendChild the image to our card
 };
 
 // function to call when form input is given focus
