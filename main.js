835f48bd99b069836c949620b00b7a98a338c8d4
Andrea <giovanniroy49@gmail.com>
2023-07-23 16:20:57 -0400

    fetch meal categories data

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const searchTermsInput = document.body.querySelector("#search-terms");
 
+const getMealCategories = async () => {
+  const mealCategoriesApiURl =
+    "https://www.themealdb.com/api/json/v1/1/categories.php";
 
-const handleFormInputFocus = () => {
-    console.log('focus occurred')
+  try {
+    const response = await fetch(mealCategoriesApiURL);
+    const data = response.json();
+    console.log("data: ", data);
+  } catch (error) {
+    console.log(error);
+    alert("Something went wrong, try again later");
 }
 const handleFormInputFocus = async () => {
   console.log("focus occurred");
