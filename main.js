c05f1ac208095e21d9df0104e5b6b67a520f60af
Andrea <giovanniroy49@gmail.com>
2023-07-23 17:50:54 -0400

    add function to render air quality categories

--- main.js
+++ main.js
@@ -3,13 +3,12 @@ console.log("main.js connected");
 // get search field input
 const searchTermsInput = document.body.querySelector("#search-terms");
 
-// fetch meal categories data from the MealDB API
-const getMealCategories = async () => {
-  const mealCategoriesApiURl =
-    "https://www.themealdb.com/api/json/v1/1/categories.php";
+// fetch air quality categories data from the AQICN API
+const getAirCategories = async () => {
+  const airCategoriesApiURL = "https://aqicn.org/api/";
 
   try {
-    const response = await fetch(mealCategoriesApiURL);
+    const response = await fetch(airCategoriesApiURL);
     const data = await response.json();
     const categories = data.categories;
     console.log(`categories: `, categories);
@@ -18,12 +17,11 @@ const getMealCategories = async () => {
     console.log(error);
     alert("Something went wrong, try again later");
   }
-  
 };
 
-// render meal categories data
-const renderMealCategories = (mealCategoriesObj) => {
-  console.log("renderMealCategories");
+// render air categories data
+const airCategories = (airCategoriesObj) => {
+  console.log("renderAirCategories");
   console.table(mealCategoriesObj);
 };
 
@@ -31,8 +29,8 @@ const renderMealCategories = (mealCategoriesObj) => {
 const handleFormInputFocus = async () => {
   console.log("focus occurred");
 
-  const mealCategoriesObj = await getMealCategories();
-  renderMealCategories(mealCategoriesObj);
+  const mealCategoriesObj = await getAirCategories();
+  renderAirCategories(airCategoriesObj);
 };
 
 // add event listener to search term
